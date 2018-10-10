a87064bfb9b4ae802272205f9e47f26270278d55
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-10-10 11:32:18 +0800

    add test dependency
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,9 @@ plugins {
 repositories {
     jcenter()
 }
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:+")
+    testImplementation("org.assertj:assertj-core:+")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:+")
+}
--- src/test/kotlin/buildSrc/GradleTest.kt
+++ src/test/kotlin/buildSrc/GradleTest.kt
@@ -1,8 +1,25 @@
 package buildSrc
 
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import kotlin.reflect.full.declaredMemberExtensionProperties
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.declaredMembers
+
 internal class GradleTest {
 
+    @Test
     fun `all dependencies`() {
+        println("declaredmembers")
+        Gradle::class.declaredMembers.forEach(::println)
+        println("declaredMemberProperties")
+        Gradle::class.declaredMemberProperties.forEach(::println)
+        println("declaredMemberExtensionProperties")
+        Gradle::class.declaredMemberExtensionProperties.forEach(::println)
+        assertAll(
+                        { Assertions.assertThat(`assertj-core`).isNotNull().isNotBlank()}
+        )
         println(`assertj-core`)
         println(`aws-java-sdk-bom`)
     }
