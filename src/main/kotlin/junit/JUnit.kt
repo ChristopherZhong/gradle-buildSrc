c55def35131c04a1c6ccb740eb40a5845050edb3
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-13 12:31:59 +0800

    add JUnit 4
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

---
+++ src/main/kotlin/junit/JUnit.kt
@@ -0,0 +1,10 @@
+package junit
+
+object JUnit {
+    const val GROUP = "junit"
+    const val VERSION = "4.12"
+    // libraries below
+    const val JUNIT = "junit"
+}
+
+const val junit = "${JUnit.GROUP}:${JUnit.JUNIT}:${JUnit.VERSION}"
