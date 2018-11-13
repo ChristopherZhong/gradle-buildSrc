5920c6147e239e76d1d2e152d2c0848023c4d51c
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-13 15:33:48 +0800

    add Truth extensions library
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/com/google/truth/Truth.kt
+++ src/main/kotlin/com/google/truth/Truth.kt
@@ -5,6 +5,13 @@ object Truth {
     const val VERSION = "0.42"
     // libraries below
     const val TRUTH = "truth"
+
+    object Extensions {
+        const val GROUP = "${Truth.GROUP}.extensions"
+        // libraries below
+        const val `JAVA8-EXTENSION` = "truth-java8-extension"
+    }
 }
 
 const val truth = "${Truth.GROUP}:${Truth.TRUTH}:${Truth.VERSION}"
+const val `truth-java8-extension` = "${Truth.Extensions.GROUP}:${Truth.Extensions.`JAVA8-EXTENSION`}"
