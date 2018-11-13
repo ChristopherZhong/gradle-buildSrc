884cfeb6a52da3e6c40b533886db42686e4dff95
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-13 12:59:01 +0800

    add JMockit
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

---
+++ src/main/kotlin/org/jmockit/JMockit.kt
@@ -0,0 +1,10 @@
+package org.jmockit
+
+object JMockit {
+    const val GROUP = "org.jmockit"
+    const val VERSION = "1.44"
+    // libraries below
+    const val JMOCKIT = "jmockit"
+}
+
+const val jmockit = "${JMockit.GROUP}:${JMockit.JMOCKIT}:${JMockit.VERSION}"
