28804b66bf95fa1908f724e7058b9e890db83737
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-13 11:11:37 +0800

    add a version variable for Spring dependency management
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/SpringDependencyManagement.kt
+++ src/main/kotlin/SpringDependencyManagement.kt
@@ -1,6 +1,9 @@
 import io.spring.DependencyManagement
+import org.gradle.kotlin.dsl.version
 import org.gradle.plugin.use.PluginDependenciesSpec
 import org.gradle.plugin.use.PluginDependencySpec
 
 inline val PluginDependenciesSpec.`spring-dependency-management`: PluginDependencySpec
     get() = id(DependencyManagement.ID)
+inline val PluginDependenciesSpec.`spring-dependency-management-version`: PluginDependencySpec
+    get() = `spring-dependency-management` version DependencyManagement.VERSION
