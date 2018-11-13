d5c98ad658d64b5c9367734fb7f293df0d75e670
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-13 15:12:43 +0800

    add Truth library
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

---
+++ src/main/kotlin/com/google/truth/Truth.kt
@@ -0,0 +1,10 @@
+package com.google.truth
+
+object Truth {
+    const val GROUP = "com.google.truth"
+    const val VERSION = "0.42"
+    // libraries below
+    const val TRUTH = "truth"
+}
+
+const val truth = "${Truth.GROUP}:${Truth.TRUTH}:${Truth.VERSION}"
