edefa347fff73a992342bdc2db396ec5a2f6941c
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-14 17:42:07 +0800

    add JavaFX for cross-platform libraries (linux, mac, win)
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/org/openjfx/JavaFX.kt
+++ src/main/kotlin/org/openjfx/JavaFX.kt
@@ -10,5 +10,14 @@ object JavaFX {
 }
 
 const val `javafx-base` = "${JavaFX.GROUP}:${JavaFX.BASE}:${JavaFX.VERSION}"
+const val `javafx-base-linux` = "${`javafx-base`}:linux"
+const val `javafx-base-mac` = "${`javafx-base`}:mac"
+const val `javafx-base-win` = "${`javafx-base`}:win"
 const val `javafx-controls` = "${JavaFX.GROUP}:${JavaFX.CONTROLS}:${JavaFX.VERSION}"
+const val `javafx-controls-linux` = "${`javafx-controls`}:linux"
+const val `javafx-controls-mac` = "${`javafx-controls`}:mac"
+const val `javafx-controls-win` = "${`javafx-controls`}:win"
 const val `javafx-graphics` = "${JavaFX.GROUP}:${JavaFX.GRAPHICS}:${JavaFX.VERSION}"
+const val `javafx-graphics-linux` = "${`javafx-graphics`}:linux"
+const val `javafx-graphics-mac` = "${`javafx-graphics`}:mac"
+const val `javafx-graphics-win` = "${`javafx-graphics`}:win"
