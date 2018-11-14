f0ad99b843c1a8dcd96a93ca995de68d4e917c87
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-14 17:29:35 +0800

    add JavaFX
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

---
+++ src/main/kotlin/org/openjfx/JavaFX.kt
@@ -0,0 +1,14 @@
+package org.openjfx
+
+object JavaFX {
+    const val GROUP = "org.openjfx"
+    const val VERSION = "11.0.1"
+    // libraries below
+    const val BASE = "javafx-base"
+    const val CONTROLS = "javafx-controls"
+    const val GRAPHICS = "javafx-graphics"
+}
+
+const val `javafx-base` = "${JavaFX.GROUP}:${JavaFX.BASE}:${JavaFX.VERSION}"
+const val `javafx-controls` = "${JavaFX.GROUP}:${JavaFX.CONTROLS}:${JavaFX.VERSION}"
+const val `javafx-graphics` = "${JavaFX.GROUP}:${JavaFX.GRAPHICS}:${JavaFX.VERSION}"
