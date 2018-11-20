df765ff48a9216effe457bb8cff90cef56ab8c49
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-20 12:18:00 +0800

    add Mockito extensions for JUnit 5
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/org/mockito/Mockito.kt
+++ src/main/kotlin/org/mockito/Mockito.kt
@@ -5,6 +5,8 @@ object Mockito {
     const val VERSION = "2.23.0"
     // libraries below
     const val CORE = "mockito-core"
+    const val JUNIT_JUPITER = "mockito-junit-jupiter"
 }
 
 const val `mockito-core`: String = "${Mockito.GROUP}:${Mockito.CORE}:${Mockito.VERSION}"
+const val `mockito-junit-jupiter` = "${Mockito.GROUP}:${Mockito.JUNIT_JUPITER}:${Mockito.VERSION}"
