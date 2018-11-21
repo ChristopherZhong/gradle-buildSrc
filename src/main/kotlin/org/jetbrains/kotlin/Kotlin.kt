b905183fb593df10523009fc99abb13a31aadfac
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-21 13:54:11 +0800

    update Kotlin to 1.3.10
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/org/jetbrains/kotlin/Kotlin.kt
+++ src/main/kotlin/org/jetbrains/kotlin/Kotlin.kt
@@ -2,7 +2,7 @@ package org.jetbrains.kotlin
 
 object Kotlin {
     const val ID = "org.jetbrains.kotlin"
-    const val VERSION = "1.3.0"
+    const val VERSION = "1.3.10"
     const val JVM = "$ID.jvm"
     const val KAPT = "$ID.kapt"
 }
