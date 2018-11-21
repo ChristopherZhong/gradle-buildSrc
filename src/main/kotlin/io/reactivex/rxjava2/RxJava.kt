6b7c7c4bd5cfc4370f4c36600883608067f3ff81
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-21 14:00:23 +0800

    add RxJava
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

---
+++ src/main/kotlin/io/reactivex/rxjava2/RxJava.kt
@@ -0,0 +1,10 @@
+package io.reactivex.rxjava2
+
+object RxJava {
+    const val GROUP = "io.reactivex.rxjava2"
+    const val VERSION = "2.2.3"
+    // libraries below
+    const val RXJAVA = "rxjava"
+}
+
+const val rxjava = "${RxJava.GROUP}:${RxJava.RXJAVA}:${RxJava.VERSION}"
