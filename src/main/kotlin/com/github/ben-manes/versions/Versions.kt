cfd4878aa3569a4f7dbeadf2f5cf9c51bc5a0ced
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-21 13:45:08 +0800

    add BenManes Gradle Versions plugin
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

---
+++ src/main/kotlin/com/github/ben-manes/versions/Versions.kt
@@ -0,0 +1,8 @@
+package com.github.`ben-manes`.versions
+
+object Versions{
+    const val ID = "com.github.ben-manes.versions"
+    const val VERSION = "0.20.0"
+    // libraries below
+    const val NAME = "gradle-versions-plugin"
+}
