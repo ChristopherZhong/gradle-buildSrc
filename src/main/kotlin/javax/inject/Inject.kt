f390a7b65517feeaaa72fc5b4fe9b0c625d4b31f
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-25 10:22:00 +0800

    add javax.inject
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

---
+++ src/main/kotlin/javax/inject/Inject.kt
@@ -0,0 +1,10 @@
+package javax.inject
+
+object Inject {
+    const val GROUP = ""
+    const val VERSION = "1"
+    // libraries below
+    const val INJECT = "javax.inject"
+}
+
+const val `javax_inject` = "${Inject.GROUP}:${Inject.INJECT}:${Inject.VERSION}"
