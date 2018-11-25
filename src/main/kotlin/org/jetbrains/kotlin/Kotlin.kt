e248b15df9a06c069a8ea7f1872c255a36b5ab62
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-25 10:06:01 +0800

    add Kotlin libraries
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/org/jetbrains/kotlin/Kotlin.kt
+++ src/main/kotlin/org/jetbrains/kotlin/Kotlin.kt
@@ -1,8 +1,19 @@
 package org.jetbrains.kotlin
 
 object Kotlin {
-    const val ID = "org.jetbrains.kotlin"
+    const val GROUP = "org.jetbrains.kotlin"
     const val VERSION = "1.3.10"
+    const val ID = GROUP
     const val JVM = "$ID.jvm"
     const val KAPT = "$ID.kapt"
+    // libraries below
+    const val STDLIB = "kotlin-stdlib"
+    const val `STDLIB-JDK8` = "$STDLIB-jdk8"
+    const val TEST = "kotlin-test"
+    const val `TEST-JUNIT5` = "$TEST-junit5"
 }
+
+const val `kotlin-stdlib` = "${Kotlin.GROUP}:${Kotlin.`STDLIB`}:${Kotlin.VERSION}"
+const val `kotlin-stdlib-jdk8` = "${Kotlin.GROUP}:${Kotlin.`STDLIB-JDK8`}:${Kotlin.VERSION}"
+const val `kotlin-test` = "${Kotlin.GROUP}:${Kotlin.TEST}:${Kotlin.VERSION}"
+const val `kotlin-test-junit5` = "${Kotlin.GROUP}:${Kotlin.`TEST-JUNIT5`}:${Kotlin.VERSION}"
