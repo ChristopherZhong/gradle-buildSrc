a03d0b3199fc2056c2c79024461537cc26c1d9df
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-25 10:11:40 +0800

    add SLF4J
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

---
+++ src/main/kotlin/org/slf4j/SLF4J.kt
@@ -0,0 +1,10 @@
+package org.slf4j
+
+object SLF4J {
+    const val GROUP = "org.slf4j"
+    const val VERSION = "1.7.25"
+    // libraries below
+    const val API = "slf4j-api"
+}
+
+const val `slf4j-api` = "${SLF4J.GROUP}:${SLF4J.API}:${SLF4J.VERSION}"
