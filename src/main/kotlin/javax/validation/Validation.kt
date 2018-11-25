947649db3884736ce9128a5bb6af3d4fa19ef912
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-25 10:26:12 +0800

    add javax.validation
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

---
+++ src/main/kotlin/javax/validation/Validation.kt
@@ -0,0 +1,10 @@
+package javax.validation
+
+object Validation {
+    const val GROUP = "javax.validation"
+    const val VERSION = "2.0.1.Final"
+    // libraries below
+    const val API = "validation-api"
+}
+
+const val `validation-api` = "${Validation.GROUP}:${Validation.API}:${Validation.VERSION}"
