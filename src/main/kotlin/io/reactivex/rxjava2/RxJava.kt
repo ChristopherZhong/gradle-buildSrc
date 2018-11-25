06baa52b1df476d0182da91ebda98a78bdec867e
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-25 09:41:44 +0800

    update RxJava to 2.2.4
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/io/reactivex/rxjava2/RxJava.kt
+++ src/main/kotlin/io/reactivex/rxjava2/RxJava.kt
@@ -2,7 +2,7 @@ package io.reactivex.rxjava2
 
 object RxJava {
     const val GROUP = "io.reactivex.rxjava2"
-    const val VERSION = "2.2.3"
+    const val VERSION = "2.2.4"
     // libraries below
     const val RXJAVA = "rxjava"
 }
