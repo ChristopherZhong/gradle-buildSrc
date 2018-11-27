3db02c74c64defff2e6bc78c2e9b4dc98f457bcc
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-11-27 15:03:14 +0800

    add Eclipse annotation library
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

---
+++ src/main/kotlin/org/eclipse/jdt/Annotation.kt
@@ -0,0 +1,10 @@
+package org.eclipse.jdt
+
+object Annotation {
+    const val GROUP = "org.eclipse.jdt"
+    const val VERSION = "2.2.100"
+    // libraries below
+    const val ANNOTATION = "org.eclipse.jdt.annotation"
+}
+
+const val `org-eclipse-jdt-annotation` = "${Annotation.GROUP}:${Annotation.ANNOTATION}:${Annotation.VERSION}"
