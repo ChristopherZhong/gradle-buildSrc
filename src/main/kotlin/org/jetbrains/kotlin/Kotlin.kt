d13a6e2a5c514c719f3f4c51161d258f2b96a09d
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-12-07 11:23:28 +0800

    bump Kotlin to 1.3.11
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/org/jetbrains/kotlin/Kotlin.kt
+++ src/main/kotlin/org/jetbrains/kotlin/Kotlin.kt
@@ -2,7 +2,7 @@ package org.jetbrains.kotlin
 
 object Kotlin {
     const val GROUP = "org.jetbrains.kotlin"
-    const val VERSION = "1.3.10"
+    const val VERSION = "1.3.11"
     const val ID = GROUP
     const val JVM = "$ID.jvm"
     const val KAPT = "$ID.kapt"
@@ -13,7 +13,7 @@ object Kotlin {
     const val `TEST-JUNIT5` = "$TEST-junit5"
 }
 
-const val `kotlin-stdlib` = "${Kotlin.GROUP}:${Kotlin.`STDLIB`}:${Kotlin.VERSION}"
+const val `kotlin-stdlib` = "${Kotlin.GROUP}:${Kotlin.STDLIB}:${Kotlin.VERSION}"
 const val `kotlin-stdlib-jdk8` = "${Kotlin.GROUP}:${Kotlin.`STDLIB-JDK8`}:${Kotlin.VERSION}"
 const val `kotlin-test` = "${Kotlin.GROUP}:${Kotlin.TEST}:${Kotlin.VERSION}"
 const val `kotlin-test-junit5` = "${Kotlin.GROUP}:${Kotlin.`TEST-JUNIT5`}:${Kotlin.VERSION}"
