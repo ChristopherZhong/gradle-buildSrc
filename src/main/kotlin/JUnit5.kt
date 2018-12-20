c5d7262b1db414f8a2a89ad759b26f0ef0c27581
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-12-20 18:42:33 +0800

    narrow the scope to just dependencies
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/JUnit5.kt
+++ src/main/kotlin/JUnit5.kt
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.DependencyHandlerScope
+
 object JUnit5 {
     const val GROUP = "org.junit"
     const val VERSION = "5.3.2"
@@ -7,7 +9,8 @@ object JUnit5 {
     }
 }
 
-const val `junit-bom` = "${JUnit5.GROUP}:${JUnit5.Dependencies.BOM}:${JUnit5.VERSION}"
+val DependencyHandlerScope.`junit-bom`: String
+    get() = "${JUnit5.GROUP}:${JUnit5.Dependencies.BOM}:${JUnit5.VERSION}"
 
 object JUnit5_Jupiter {
     const val GROUP = "${JUnit5.GROUP}.jupiter"
@@ -19,9 +22,12 @@ object JUnit5_Jupiter {
     }
 }
 
-const val `junit-jupiter-api` = "${JUnit5_Jupiter.GROUP}:${JUnit5_Jupiter.Dependencies.API}:${JUnit5.VERSION}"
-const val `junit-jupiter-engine` = "${JUnit5_Jupiter.GROUP}:${JUnit5_Jupiter.Dependencies.ENGINE}:${JUnit5.VERSION}"
-const val `junit-jupiter-params` = "${JUnit5_Jupiter.GROUP}:${JUnit5_Jupiter.Dependencies.PARAMS}:${JUnit5.VERSION}"
+val DependencyHandlerScope.`junit-jupiter-api`: String
+    get() = "${JUnit5_Jupiter.GROUP}:${JUnit5_Jupiter.Dependencies.API}:${JUnit5.VERSION}"
+val DependencyHandlerScope.`junit-jupiter-engine`: String
+    get() = "${JUnit5_Jupiter.GROUP}:${JUnit5_Jupiter.Dependencies.ENGINE}:${JUnit5.VERSION}"
+val DependencyHandlerScope.`junit-jupiter-params`: String
+    get() = "${JUnit5_Jupiter.GROUP}:${JUnit5_Jupiter.Dependencies.PARAMS}:${JUnit5.VERSION}"
 
 object JUnit5_Vintage {
     const val GROUP = "${JUnit5.GROUP}.vintage"
@@ -31,4 +37,5 @@ object JUnit5_Vintage {
     }
 }
 
-const val `junit-vintage-engine` = "${JUnit5_Vintage.GROUP}:${JUnit5_Vintage.Dependencies.ENGINE}:${JUnit5.VERSION}"
+val DependencyHandlerScope.`junit-vintage-engine`: String
+    get() = "${JUnit5_Vintage.GROUP}:${JUnit5_Vintage.Dependencies.ENGINE}:${JUnit5.VERSION}"
