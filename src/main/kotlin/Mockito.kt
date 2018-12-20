46105a16aab343fe3b08d41799b127c167500d0a
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-12-20 18:38:22 +0800

    change scope of variable to be only in the dependencies scope
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/Mockito.kt
+++ src/main/kotlin/Mockito.kt
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.DependencyHandlerScope
+
 object Mockito {
     const val GROUP = "org.mockito"
     const val VERSION = "2.23.4"
@@ -8,5 +10,7 @@ object Mockito {
     }
 }
 
-const val `mockito-core` = "${Mockito.GROUP}:${Mockito.Dependencies.CORE}:${Mockito.VERSION}"
-const val `mockito-junit-jupiter` = "${Mockito.GROUP}:${Mockito.Dependencies.JUNIT_JUPITER}:${Mockito.VERSION}"
+val DependencyHandlerScope.`mockito-core`: String
+    get() = "${Mockito.GROUP}:${Mockito.Dependencies.CORE}:${Mockito.VERSION}"
+val DependencyHandlerScope.`mockito-junit-jupiter`: String
+    get() = "${Mockito.GROUP}:${Mockito.Dependencies.JUNIT_JUPITER}:${Mockito.VERSION}"
