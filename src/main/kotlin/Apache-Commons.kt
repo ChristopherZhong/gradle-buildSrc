3be1f79d73eff65ccdb66a6e68d587d9aba23498
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-01-10 12:00:35 +0800

    move Commons to root package
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

---
+++ src/main/kotlin/Apache-Commons.kt
@@ -0,0 +1,13 @@
+import org.gradle.api.artifacts.dsl.DependencyHandler
+
+object Commons {
+    const val GROUP = "${Apache.GROUP}.commons"
+    const val VERSION = "1.5"
+
+    object Dependencies {
+        const val CSV = "commons-csv"
+    }
+}
+
+val DependencyHandler.`commons-csv`: String
+    get() = "${Commons.GROUP}:${Commons.Dependencies.CSV}:${Commons.VERSION}"
