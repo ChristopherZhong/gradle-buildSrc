80304dca64e972f3a23090b78016fb4fa702c269
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-01-10 11:45:12 +0800

    move object to root package
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/SpringDependencyManagement.kt
+++ src/main/kotlin/SpringDependencyManagement.kt
@@ -1,12 +1,18 @@
-import io.spring.DependencyManagement
 import org.gradle.kotlin.dsl.version
 import org.gradle.plugin.use.PluginDependenciesSpec
 import org.gradle.plugin.use.PluginDependencySpec
 
-fun PluginDependenciesSpec.`spring-dependency-management`(
-    version: String = DependencyManagement.VERSION,
-    includeVersion: Boolean = true
-): PluginDependencySpec {
-    val id = id(DependencyManagement.ID)
-    return if (includeVersion) id version version else id
+object SpringDependencyManagement {
+    const val ID = "io.spring.dependency-management"
+    const val VERSION = "1.0.6.RELEASE"
 }
+
+val PluginDependenciesSpec.`spring-dependency-management`: PluginDependencySpec
+    get() = `spring-dependency-management`(SpringDependencyManagement.VERSION)
+
+fun PluginDependenciesSpec.`spring-dependency-management`(version: String? = null): PluginDependencySpec =
+        id(SpringDependencyManagement.ID).apply {
+            if (version != null) {
+                return this version version
+            }
+        }
--- src/main/kotlin/io/spring/DependencyManagement.kt
+++
@@ -1,6 +0,0 @@
-package io.spring
-
-object DependencyManagement {
-    const val ID = "io.spring.dependency-management"
-    const val VERSION = "1.0.6.RELEASE"
-}
