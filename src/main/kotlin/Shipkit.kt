08e8e74a3956dc8f7122f1752b3e5f0fbfafc15b
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-02-07 16:31:29 +0800

    bump Shipkit to 2.0.32

--- src/main/kotlin/Shipkit.kt
+++ src/main/kotlin/Shipkit.kt
@@ -4,7 +4,7 @@ import org.gradle.plugin.use.PluginDependencySpec
 
 object Shipkit {
     const val ID = "org.shipkit.java"
-    const val VERSION = "2.0.31"
+    const val VERSION = "2.0.32"
 }
 
 val PluginDependenciesSpec.`shipkit-java`: PluginDependencySpec
