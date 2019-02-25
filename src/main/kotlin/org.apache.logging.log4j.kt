459ad025092b1c5ff2d0708aa5b549717bbca393
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-02-25 12:57:30 +0800

    rename filename to match group id
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/org.apache.logging.log4j.kt
+++ src/main/kotlin/org.apache.logging.log4j.kt
@@ -4,7 +4,7 @@ object Log4j {
     const val GROUP = "${Apache.GROUP}.logging.log4j"
     const val VERSION = "2.11.2"
 
-    internal object Dependencies {
+    internal object Dependency {
         const val `1_2_API` = "log4j-1.2-api"
         const val API = "log4j-api"
         const val BOM = "log4j-bom"
@@ -15,14 +15,14 @@ object Log4j {
 }
 
 val DependencyHandler.`log4j-1_2-api`: String
-    get() = "${Log4j.GROUP}:${Log4j.Dependencies.`1_2_API`}:${Log4j.VERSION}"
+    get() = "${Log4j.GROUP}:${Log4j.Dependency.`1_2_API`}:${Log4j.VERSION}"
 val DependencyHandler.`log4j-api`: String
-    get() = "${Log4j.GROUP}:${Log4j.Dependencies.API}:${Log4j.VERSION}"
+    get() = "${Log4j.GROUP}:${Log4j.Dependency.API}:${Log4j.VERSION}"
 val DependencyHandler.`log4j-bom`: String
-    get() = "${Log4j.GROUP}:${Log4j.Dependencies.BOM}:${Log4j.VERSION}"
+    get() = "${Log4j.GROUP}:${Log4j.Dependency.BOM}:${Log4j.VERSION}"
 val DependencyHandler.`log4j-core`: String
-    get() = "${Log4j.GROUP}:${Log4j.Dependencies.CORE}:${Log4j.VERSION}"
+    get() = "${Log4j.GROUP}:${Log4j.Dependency.CORE}:${Log4j.VERSION}"
 val DependencyHandler.`log4j-jcl`: String
-    get() = "${Log4j.GROUP}:${Log4j.Dependencies.JCL}:${Log4j.VERSION}"
+    get() = "${Log4j.GROUP}:${Log4j.Dependency.JCL}:${Log4j.VERSION}"
 val DependencyHandler.`log4j-slf4j-impl`: String
-    get() = "${Log4j.GROUP}:${Log4j.Dependencies.SLF4J_IMPL}:${Log4j.VERSION}"
+    get() = "${Log4j.GROUP}:${Log4j.Dependency.SLF4J_IMPL}:${Log4j.VERSION}"
