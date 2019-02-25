657cdacaeddbdd6c9f6c703aadf0b19bac38e5f9
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-02-25 12:52:22 +0800

    add commons-net
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/Apache-Commons.kt
+++ src/main/kotlin/Apache-Commons.kt
@@ -2,12 +2,16 @@ import org.gradle.api.artifacts.dsl.DependencyHandler
 
 object Commons {
     const val GROUP = "${Apache.GROUP}.commons"
-    const val VERSION = "1.6"
 
-    object Dependencies {
+    object Dependency {
         const val CSV = "commons-csv"
+        const val CSV_VERSION = "1.6"
+        const val NET = "commons-net"
+        const val NET_VERSION = "3.6"
     }
 }
 
 val DependencyHandler.`commons-csv`: String
-    get() = "${Commons.GROUP}:${Commons.Dependencies.CSV}:${Commons.VERSION}"
+    get() = "${Commons.GROUP}:${Commons.Dependency.CSV}:${Commons.Dependency.CSV_VERSION}"
+val DependencyHandler.`commons-net`: String
+    get() = "${Commons.Dependency.NET}:${Commons.Dependency.NET}:${Commons.Dependency.NET_VERSION}"
