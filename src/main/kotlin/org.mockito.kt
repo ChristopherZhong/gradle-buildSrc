158ff34c0cd4a7945f97e394f7bd86d35529d324
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-02-25 12:42:46 +0800

    rename object to be singular
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/org.mockito.kt
+++ src/main/kotlin/org.mockito.kt
@@ -4,13 +4,13 @@ object Mockito {
     const val GROUP = "org.mockito"
     const val VERSION = "2.24.0"
 
-    internal object Dependencies {
+    internal object Dependency {
         const val CORE = "mockito-core"
         const val JUNIT_JUPITER = "mockito-junit-jupiter"
     }
 }
 
 val DependencyHandler.`mockito-core`: String
-    get() = "${Mockito.GROUP}:${Mockito.Dependencies.CORE}:${Mockito.VERSION}"
+    get() = "${Mockito.GROUP}:${Mockito.Dependency.CORE}:${Mockito.VERSION}"
 val DependencyHandler.`mockito-junit-jupiter`: String
-    get() = "${Mockito.GROUP}:${Mockito.Dependencies.JUNIT_JUPITER}:${Mockito.VERSION}"
+    get() = "${Mockito.GROUP}:${Mockito.Dependency.JUNIT_JUPITER}:${Mockito.VERSION}"
