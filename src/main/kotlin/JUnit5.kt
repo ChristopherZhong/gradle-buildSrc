6e2e33209662dd7d76adca8ef4c4df439d6dcef6
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-02-25 12:43:30 +0800

    bump JUnit to 5.4.0
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/JUnit5.kt
+++ src/main/kotlin/JUnit5.kt
@@ -2,7 +2,7 @@ import org.gradle.api.artifacts.dsl.DependencyHandler
 
 object JUnit5 {
     const val GROUP = "org.junit"
-    const val VERSION = "5.3.2"
+    const val VERSION = "5.4.0"
 
     internal object Dependencies {
         const val BOM = "junit-bom"
