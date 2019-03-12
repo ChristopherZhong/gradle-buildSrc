e6c785cccd65d1b48ccc1871a6052379cf367551
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-12 14:46:16 +0800

    inline variable that are used once except for version
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/com.netflix.nebula.kt
+++ src/main/kotlin/com.netflix.nebula.kt
@@ -9,7 +9,6 @@ object Nebula {
 }
 
 object NebulaKotlin {
-    const val ID = "nebula.kotlin"
     const val VERSION = "1.3.21"
 }
 
@@ -17,7 +16,7 @@ val PluginDependenciesSpec.`nebula-kotlin`: PluginDependencySpec
     get() = `nebula-kotlin`(NebulaKotlin.VERSION)
 
 fun PluginDependenciesSpec.`nebula-kotlin`(version: String? = null): PluginDependencySpec =
-        id(NebulaKotlin.ID).apply {
+        id("nebula.kotlin").apply {
             if (version != null) {
                 return this version version
             }
@@ -25,18 +24,6 @@ fun PluginDependenciesSpec.`nebula-kotlin`(version: String? = null): PluginDepen
 
 object NebulaPublishing {
     const val VERSION = "9.4.5"
-
-    const val MAVEN_BASE_PUBLISH = "nebula.maven-base-publish"
-    const val MAVEN_DEPENDENCIES = "nebula.maven-dependencies"
-    const val MAVEN_PUBLISH = "nebula.maven-publish"
-    const val MAVEN_MANIFEST = "nebula.maven-manifest"
-    const val MAVEN_RESOLVED_DEPENDENCIES = "nebula.maven-resolved-dependencies"
-    const val MAVEN_SCM = "nebula.maven-scm"
-
-    const val COMPILE_API = "nebula.compile-api"
-    const val JAVADOC_JAR = "nebula.javadoc-jar"
-    const val SOURCE_JAR = "nebula.source-jar"
-    const val PUBLISH_VERIFICATION = "nebula.publish-verification"
 }
 
 val PluginDependenciesSpec.`nebula-maven-publish`: PluginDependencySpec
@@ -49,35 +36,34 @@ val PluginDependenciesSpec.`nebula-publish-verification`: PluginDependencySpec
     get() = `nebula-publish-verification`(NebulaPublishing.VERSION)
 
 fun PluginDependenciesSpec.`nebula-maven-publish`(version: String? = null): PluginDependencySpec =
-        id(NebulaPublishing.MAVEN_PUBLISH).apply {
+        id("nebula.maven-publish").apply {
             if (version != null) {
                 return this version version
             }
         }
 
 fun PluginDependenciesSpec.`nebula-javadoc-jar`(version: String? = null): PluginDependencySpec =
-        id(NebulaPublishing.JAVADOC_JAR).apply {
+        id("nebula.javadoc-jar").apply {
             if (version != null) {
                 return this version version
             }
         }
 
 fun PluginDependenciesSpec.`nebula-source-jar`(version: String? = null): PluginDependencySpec =
-        id(NebulaPublishing.SOURCE_JAR).apply {
+        id("nebula.source-jar").apply {
             if (version != null) {
                 return this version version
             }
         }
 
 fun PluginDependenciesSpec.`nebula-publish-verification`(version: String? = null): PluginDependencySpec =
-        id(NebulaPublishing.PUBLISH_VERIFICATION).apply {
+        id("nebula.publish-verification").apply {
             if (version != null) {
                 return this version version
             }
         }
 
 object NebulaRelease {
-    const val ID = "nebula.release"
     const val VERSION = "9.2.0"
 }
 
@@ -85,7 +71,7 @@ val PluginDependenciesSpec.`nebula-release`: PluginDependencySpec
     get() = `nebula-release`(NebulaRelease.VERSION)
 
 fun PluginDependenciesSpec.`nebula-release`(version: String? = null): PluginDependencySpec =
-        id(NebulaRelease.ID).apply {
+        id("nebula.release").apply {
             if (version != null) {
                 return this version version
             }
