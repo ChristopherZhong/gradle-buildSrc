67a72877907b35a7d931052e0aa714d707d172ea
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-12 14:54:40 +0800

    rename filename to match GROUP and suppress warnings
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/org.projectlombok.kt
+++ src/main/kotlin/org.projectlombok.kt
@@ -1,13 +1,11 @@
+@file:Suppress("unused")
+
 import org.gradle.api.artifacts.dsl.DependencyHandler
 
 object Lombok {
     const val GROUP = "org.projectlombok"
     const val VERSION = "1.18.4"
-
-    object Dependencies {
-        const val LOMBOK = "lombok"
-    }
 }
 
 val DependencyHandler.lombok: String
-    get() = "${Lombok.GROUP}:${Lombok.Dependencies.LOMBOK}:${Lombok.VERSION}"
+    get() = "${Lombok.GROUP}:lombok:${Lombok.VERSION}"
