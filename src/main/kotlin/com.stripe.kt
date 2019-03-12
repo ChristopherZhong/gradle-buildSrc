dbc98ecf06c042116eec3e85b353a291d28d8044
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-12 11:45:31 +0800

    add Stripe dependency
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

---
+++ src/main/kotlin/com.stripe.kt
@@ -0,0 +1,9 @@
+import org.gradle.api.artifacts.dsl.DependencyHandler
+
+object Stripe {
+    const val GROUP = "com.stripe"
+    const val VERSION = "7.0.0"
+}
+
+val DependencyHandler.`stripe-java`: String
+    get() = "${Stripe.GROUP}:stripe-java:${Stripe.VERSION}"
