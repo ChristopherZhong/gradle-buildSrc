53e93794e2d70723ecc57c059adabc49c7eb1736
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-27 10:30:11 +0800

    collapse inner object `Dependencies`
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/com.amazonaws.kt
+++ src/main/kotlin/com.amazonaws.kt
@@ -8,30 +8,25 @@ object Aws {
 
 object AwsJava {
     const val VERSION = "1.11.414"
-
-    internal object Dependencies {
-        const val SDK = "aws-java-sdk"
-    }
+    const val SDK = "aws-java-sdk"
 }
 
 val DependencyHandler.`aws-java-sdk-bom`: String
-    get() = "${Aws.GROUP}:${AwsJava.Dependencies.SDK}-bom:${AwsJava.VERSION}"
+    get() = "${Aws.GROUP}:${AwsJava.SDK}-bom:${AwsJava.VERSION}"
 val DependencyHandler.`aws-java-sdk-dynamodb`: String
-    get() = "${Aws.GROUP}:${AwsJava.Dependencies.SDK}-dynamodb:${AwsJava.VERSION}"
+    get() = "${Aws.GROUP}:${AwsJava.SDK}-dynamodb:${AwsJava.VERSION}"
 val DependencyHandler.`aws-java-sdk-ecs`: String
-    get() = "${Aws.GROUP}:${AwsJava.Dependencies.SDK}-ecs:${AwsJava.VERSION}"
+    get() = "${Aws.GROUP}:${AwsJava.SDK}-ecs:${AwsJava.VERSION}"
 val DependencyHandler.`aws-java-sdk-s3`: String
-    get() = "${Aws.GROUP}:${AwsJava.Dependencies.SDK}-s3:${AwsJava.VERSION}"
+    get() = "${Aws.GROUP}:${AwsJava.SDK}-s3:${AwsJava.VERSION}"
 
 object AwsLambda {
-    internal object Dependencies {
-        const val JAVA = "aws-lambda-java"
-    }
+    const val JAVA = "aws-lambda-java"
 }
 
 val DependencyHandler.`aws-lambda-java-core`: String
-    get() = "${Aws.GROUP}:${AwsLambda.Dependencies.JAVA}-core:1.2.0"
+    get() = "${Aws.GROUP}:${AwsLambda.JAVA}-core:1.2.0"
 val DependencyHandler.`aws-lambda-java-events`: String
-    get() = "${Aws.GROUP}:${AwsLambda.Dependencies.JAVA}-events:2.2.2"
+    get() = "${Aws.GROUP}:${AwsLambda.JAVA}-events:2.2.2"
 val DependencyHandler.`aws-lambda-java-log4j2`: String
-    get() = "${Aws.GROUP}:${AwsLambda.Dependencies.JAVA}-log4j2:1.1.0"
+    get() = "${Aws.GROUP}:${AwsLambda.JAVA}-log4j2:1.1.0"
