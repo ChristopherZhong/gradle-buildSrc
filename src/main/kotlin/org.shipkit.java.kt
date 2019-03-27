f5394f902965444c4f0a4e78dcf6cc087a73540e
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-27 11:30:21 +0800

    use extension function `applyVersion` and inline variables
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/org.shipkit.java.kt
+++ src/main/kotlin/org.shipkit.java.kt
@@ -1,18 +1,8 @@
-import org.gradle.kotlin.dsl.version
 import org.gradle.plugin.use.PluginDependenciesSpec
 import org.gradle.plugin.use.PluginDependencySpec
 
-object Shipkit {
-    const val ID = "org.shipkit.java"
-    const val VERSION = "2.1.3"
-}
-
 val PluginDependenciesSpec.`shipkit-java`: PluginDependencySpec
-    get() = `shipkit-java`(Shipkit.VERSION)
+    get() = `shipkit-java`("2.1.3")
 
 fun PluginDependenciesSpec.`shipkit-java`(version: String? = null): PluginDependencySpec =
-        id(Shipkit.ID).apply {
-            if (version != null) {
-                return this version version
-            }
-        }
+    id("org.shipkit.java").applyVersion(version)
