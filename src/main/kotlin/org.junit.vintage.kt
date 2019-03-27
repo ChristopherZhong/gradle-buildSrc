83874e3ec29ca1d060684ec8fe8d12e7f463ca77
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-27 11:17:43 +0800

    inline variable `GROUP`
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/org.junit.vintage.kt
+++ src/main/kotlin/org.junit.vintage.kt
@@ -2,9 +2,5 @@
 
 import org.gradle.api.artifacts.dsl.DependencyHandler
 
-object JUnit5Vintage {
-    const val GROUP = "${JUnit5.GROUP}.vintage"
-}
-
 val DependencyHandler.`junit-vintage-engine`: String
-    get() = "${JUnit5Vintage.GROUP}:${JUnit5.JUNIT}-vintage-engine:${JUnit5.VERSION}"
+    get() = "${JUnit5.GROUP}.vintage:${JUnit5.JUNIT}-vintage-engine:${JUnit5.VERSION}"
