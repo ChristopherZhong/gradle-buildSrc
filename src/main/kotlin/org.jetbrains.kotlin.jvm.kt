9c0e16a37cb61fb92670e4b094a094fcce37d524
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-27 11:08:31 +0800

    use `applyVersion` extension function
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/org.jetbrains.kotlin.jvm.kt
+++ src/main/kotlin/org.jetbrains.kotlin.jvm.kt
@@ -1,6 +1,5 @@
 @file:Suppress("unused", "ObjectPropertyName", "FunctionName")
 
-import org.gradle.kotlin.dsl.version
 import org.gradle.plugin.use.PluginDependenciesSpec
 import org.gradle.plugin.use.PluginDependencySpec
 
@@ -12,8 +11,4 @@ val PluginDependenciesSpec.`kotlin-jvm`: PluginDependencySpec
     get() = `kotlin-jvm`(Kotlin.VERSION)
 
 fun PluginDependenciesSpec.`kotlin-jvm`(version: String? = null): PluginDependencySpec =
-        id(KotlinJvm.GROUP).apply {
-            if (version != null) {
-                return this version version
-            }
-        }
+    id(KotlinJvm.GROUP).applyVersion(version)
