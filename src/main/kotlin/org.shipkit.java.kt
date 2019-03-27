b2a2e135cbc033e3376e10bd7f1c58f0d5c20931
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-27 13:40:15 +0800

    bump shipkit to 2.2.2
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/org.shipkit.java.kt
+++ src/main/kotlin/org.shipkit.java.kt
@@ -4,7 +4,7 @@ import org.gradle.plugin.use.PluginDependenciesSpec
 import org.gradle.plugin.use.PluginDependencySpec
 
 val PluginDependenciesSpec.`shipkit-java`: PluginDependencySpec
-    get() = `shipkit-java`("2.1.3")
+    get() = `shipkit-java`("2.2.2")
 
 fun PluginDependenciesSpec.`shipkit-java`(version: String? = null): PluginDependencySpec =
     id("org.shipkit.java").applyVersion(version)
