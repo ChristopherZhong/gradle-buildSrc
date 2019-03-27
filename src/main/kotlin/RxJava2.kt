26ed57cd2967a33464fcd2ed77891ce5e433c1d8
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-27 11:27:05 +0800

    inline variables
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/RxJava2.kt
+++ src/main/kotlin/RxJava2.kt
@@ -1,13 +1,4 @@
 import org.gradle.api.artifacts.dsl.DependencyHandler
 
-object RxJava2 {
-    const val GROUP = "io.reactivex.rxjava2"
-    const val VERSION = "2.2.4"
-
-    internal object Dependencies {
-        const val RXJAVA = "rxjava"
-    }
-}
-
 val DependencyHandler.rxjava: String
-    get() = "${RxJava2.GROUP}:${RxJava2.Dependencies.RXJAVA}:${RxJava2.VERSION}"
+    get() = "io.reactivex.rxjava2:rxjava:2.2.4"
