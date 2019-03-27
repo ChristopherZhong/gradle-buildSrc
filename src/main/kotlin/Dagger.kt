00693ea7d6226edfd12af8452801d6941793e377
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-27 10:46:16 +0800

    inline `COMPILER` constant
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/Dagger.kt
+++ src/main/kotlin/Dagger.kt
@@ -3,14 +3,10 @@ import org.gradle.api.artifacts.dsl.DependencyHandler
 object Dagger {
     const val GROUP = "com.google.dagger"
     const val VERSION = "2.19"
-
-    object Dependencies {
-        const val COMPILER = "dagger-compiler"
-        const val DAGGER = "dagger"
-    }
+    const val DAGGER = "dagger"
 }
 
 val DependencyHandler.dagger: String
-    get() = "${Dagger.GROUP}:${Dagger.Dependencies.DAGGER}:${Dagger.VERSION}"
+    get() = "${Dagger.GROUP}:${Dagger.DAGGER}:${Dagger.VERSION}"
 val DependencyHandler.`dagger-compiler`: String
-    get() = "${Dagger.GROUP}:${Dagger.Dependencies.COMPILER}:${Dagger.VERSION}"
+    get() = "${Dagger.GROUP}:${Dagger.DAGGER}-compiler:${Dagger.VERSION}"
