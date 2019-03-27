69323354ca319fc728ef87d816ee1c977f564dc7
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-27 11:06:34 +0800

    inline variables
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/org.apache.httpcomponents.kt
+++ src/main/kotlin/org.apache.httpcomponents.kt
@@ -2,10 +2,5 @@
 
 import org.gradle.api.artifacts.dsl.DependencyHandler
 
-object HttpComponents {
-    const val GROUP = "${Apache.GROUP}.httpcomponents"
-    const val VERSION = "4.4.10"
-}
-
 val DependencyHandler.httpcore: String
-    get() = "${HttpComponents.GROUP}:httpcore:${HttpComponents.VERSION}"
+    get() = "${Apache.GROUP}.httpcomponents:httpcore:4.4.10"
