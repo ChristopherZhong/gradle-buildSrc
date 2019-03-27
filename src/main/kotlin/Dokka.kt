9094cd338729cb2be35e7dcb076c9feb3990f52a
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-27 13:44:08 +0800

    inline variables
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/Dokka.kt
+++ src/main/kotlin/Dokka.kt
@@ -1,13 +1,8 @@
 import org.gradle.plugin.use.PluginDependenciesSpec
 import org.gradle.plugin.use.PluginDependencySpec
 
-object Dokka {
-    const val GROUP = "org.jetbrains.dokka"
-    const val VERSION = "0.9.17"
-}
-
 val PluginDependenciesSpec.dokka: PluginDependencySpec
-    get() = dokka(Dokka.VERSION)
+    get() = dokka("0.9.17")
 
 fun PluginDependenciesSpec.dokka(version: String? = null): PluginDependencySpec =
-    id(Dokka.GROUP).applyVersion(version)
+    id("org.jetbrains.dokka").applyVersion(version)
