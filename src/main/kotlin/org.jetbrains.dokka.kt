9b149fd79a8ca99bd3a477e926e6fdb330d53f2d
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-27 13:45:07 +0800

    bump dokka to 0.9.18
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/org.jetbrains.dokka.kt
+++ src/main/kotlin/org.jetbrains.dokka.kt
@@ -2,7 +2,7 @@ import org.gradle.plugin.use.PluginDependenciesSpec
 import org.gradle.plugin.use.PluginDependencySpec
 
 val PluginDependenciesSpec.dokka: PluginDependencySpec
-    get() = dokka("0.9.17")
+    get() = dokka("0.9.18")
 
 fun PluginDependenciesSpec.dokka(version: String? = null): PluginDependencySpec =
     id("org.jetbrains.dokka").applyVersion(version)
