a84cd54dfc1bc4d89decdf70bd130aedd63b7d02
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-27 10:59:49 +0800

    extract constant `NEBULA` and use `applyVersion` extension function
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/com.netflix.nebula.kt
+++ src/main/kotlin/com.netflix.nebula.kt
@@ -1,11 +1,11 @@
 @file:Suppress("unused", "ObjectPropertyName", "FunctionName")
 
-import org.gradle.kotlin.dsl.version
 import org.gradle.plugin.use.PluginDependenciesSpec
 import org.gradle.plugin.use.PluginDependencySpec
 
 object Nebula {
     const val GROUP = "com.netflix.nebula"
+    const val NEBULA = "nebula"
 }
 
 object NebulaKotlin {
@@ -16,11 +16,7 @@ val PluginDependenciesSpec.`nebula-kotlin`: PluginDependencySpec
     get() = `nebula-kotlin`(NebulaKotlin.VERSION)
 
 fun PluginDependenciesSpec.`nebula-kotlin`(version: String? = null): PluginDependencySpec =
-        id("nebula.kotlin").apply {
-            if (version != null) {
-                return this version version
-            }
-        }
+    id("${Nebula.NEBULA}.kotlin").applyVersion(version)
 
 object NebulaPublishing {
     const val VERSION = "9.4.5"
@@ -36,32 +32,16 @@ val PluginDependenciesSpec.`nebula-publish-verification`: PluginDependencySpec
     get() = `nebula-publish-verification`(NebulaPublishing.VERSION)
 
 fun PluginDependenciesSpec.`nebula-maven-publish`(version: String? = null): PluginDependencySpec =
-        id("nebula.maven-publish").apply {
-            if (version != null) {
-                return this version version
-            }
-        }
+    id("${Nebula.NEBULA}.maven-publish").applyVersion(version)
 
 fun PluginDependenciesSpec.`nebula-javadoc-jar`(version: String? = null): PluginDependencySpec =
-        id("nebula.javadoc-jar").apply {
-            if (version != null) {
-                return this version version
-            }
-        }
+    id("${Nebula.NEBULA}.javadoc-jar").applyVersion(version)
 
 fun PluginDependenciesSpec.`nebula-source-jar`(version: String? = null): PluginDependencySpec =
-        id("nebula.source-jar").apply {
-            if (version != null) {
-                return this version version
-            }
-        }
+    id("${Nebula.NEBULA}.source-jar").applyVersion(version)
 
 fun PluginDependenciesSpec.`nebula-publish-verification`(version: String? = null): PluginDependencySpec =
-        id("nebula.publish-verification").apply {
-            if (version != null) {
-                return this version version
-            }
-        }
+    id("${Nebula.NEBULA}.publish-verification").applyVersion(version)
 
 object NebulaRelease {
     const val VERSION = "9.2.0"
@@ -71,8 +51,4 @@ val PluginDependenciesSpec.`nebula-release`: PluginDependencySpec
     get() = `nebula-release`(NebulaRelease.VERSION)
 
 fun PluginDependenciesSpec.`nebula-release`(version: String? = null): PluginDependencySpec =
-        id("nebula.release").apply {
-            if (version != null) {
-                return this version version
-            }
-        }
+    id("${Nebula.NEBULA}.release").applyVersion(version)
