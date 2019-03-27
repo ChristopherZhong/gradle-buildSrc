8ab3e72e7524d5cdff108b1cd1b8fe92bc1c202c
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-27 11:13:33 +0800

    inline variable and use extension function
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- src/main/kotlin/org.jetbrains.kotlin.kapt.kt
+++ src/main/kotlin/org.jetbrains.kotlin.kapt.kt
@@ -1,19 +1,10 @@
 @file:Suppress("unused", "ObjectPropertyName", "FunctionName")
 
-import org.gradle.kotlin.dsl.version
 import org.gradle.plugin.use.PluginDependenciesSpec
 import org.gradle.plugin.use.PluginDependencySpec
-
-object KotlinKapt {
-    const val GROUP = "${Kotlin.GROUP}.kapt"
-}
 
 val PluginDependenciesSpec.`kotlin-kapt`: PluginDependencySpec
     get() = `kotlin-kapt`(Kotlin.VERSION)
 
 fun PluginDependenciesSpec.`kotlin-kapt`(version: String? = null): PluginDependencySpec =
-        id(KotlinKapt.GROUP).apply {
-            if (version != null) {
-                this version version
-            }
-        }
+    id("${Kotlin.GROUP}.kapt").applyVersion(version)
