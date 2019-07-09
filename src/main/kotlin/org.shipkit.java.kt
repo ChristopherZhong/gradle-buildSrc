043c77abef99d656b24594f5c6a195a974b15553
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-07-09 10:31:13 +0800

    bump Shipkit to 2.2.5

--- src/main/kotlin/org.shipkit.java.kt
+++ src/main/kotlin/org.shipkit.java.kt
@@ -1,10 +1,10 @@
-@file:Suppress("ObjectPropertyName", "FunctionName")
+@file:Suppress("ObjectPropertyName", "FunctionName", "unused")
 
 import org.gradle.plugin.use.PluginDependenciesSpec
 import org.gradle.plugin.use.PluginDependencySpec
 
 val PluginDependenciesSpec.`shipkit-java`: PluginDependencySpec
-    get() = `shipkit-java`("2.2.2")
+    get() = `shipkit-java`("2.2.5")
 
 fun PluginDependenciesSpec.`shipkit-java`(version: String? = null): PluginDependencySpec =
     id("org.shipkit.java").applyVersion(version)
