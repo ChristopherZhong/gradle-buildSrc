5ccade695ef3d72496f1f28494310d5183af634d
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-07-09 12:20:07 +0800

    bump Jackson to 2.9.9

--- src/main/kotlin/com.fasterxml.jackson.kt
+++ src/main/kotlin/com.fasterxml.jackson.kt
@@ -4,7 +4,7 @@ import org.gradle.api.artifacts.dsl.DependencyHandler
 
 object Jackson {
     const val GROUP = "com.fasterxml.jackson"
-    const val VERSION = "2.9.8"
+    const val VERSION = "2.9.9"
 }
 
 val DependencyHandler.`jackson-bom`: String
