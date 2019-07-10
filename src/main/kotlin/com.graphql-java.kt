441a4c55fa3e22221a482e80fe1a9fdb38ddd227
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-07-10 10:29:47 +0800

    add graphiql library

--- src/main/kotlin/com.graphql-java.kt
+++ src/main/kotlin/com.graphql-java.kt
@@ -10,3 +10,5 @@ val DependencyHandler.`graphql-java`: String
     get() = "${GraphQLJava.GROUP}:graphql-java:13.0"
 val DependencyHandler.`graphql-java-spring-boot-starter-webflux`: String
     get() = "${GraphQLJava.GROUP}:graphql-java-spring-boot-starter-webflux:1.0"
+val DependencyHandler.`graphiql-spring-boot-starter`: String
+    get() = "${GraphQLJava.GROUP}:graphiql-spring-boot-starter:5.0.2"
