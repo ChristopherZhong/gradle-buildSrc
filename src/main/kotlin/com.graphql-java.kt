4e89c02fb8b9794c416d8dd3343dae7cab9256a7
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-07-12 14:37:39 +0800

    add GraphQL tools and spring boot starter

--- src/main/kotlin/com.graphql-java.kt
+++ src/main/kotlin/com.graphql-java.kt
@@ -8,7 +8,11 @@ object GraphQLJava {
 
 val DependencyHandler.`graphql-java`: String
     get() = "${GraphQLJava.GROUP}:graphql-java:13.0"
+val DependencyHandler.`graphql-java-tools`: String
+    get() = "${GraphQLJava.GROUP}:graphql-java-tools:5.2.4"
 val DependencyHandler.`graphql-java-spring-boot-starter-webflux`: String
     get() = "${GraphQLJava.GROUP}:graphql-java-spring-boot-starter-webflux:1.0"
+val DependencyHandler.`graphql-spring-boot-starter`: String
+    get() = "${GraphQLJava.GROUP}:graphql-spring-boot-starter:5.0.2"
 val DependencyHandler.`graphiql-spring-boot-starter`: String
     get() = "${GraphQLJava.GROUP}:graphiql-spring-boot-starter:5.0.2"
