1f8d3f0aec7e1c1947b2bae567042e4a1bfe5250
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-07-18 17:04:08 +0800

    add log4j-api-kotlin

--- src/main/kotlin/org.apache.logging.log4j.kt
+++ src/main/kotlin/org.apache.logging.log4j.kt
@@ -11,6 +11,8 @@ val DependencyHandler.`log4j-1_2-api`: String
     get() = "${Log4j.GROUP}:log4j-1.2-api:${Log4j.VERSION}"
 val DependencyHandler.`log4j-api`: String
     get() = "${Log4j.GROUP}:log4j-api:${Log4j.VERSION}"
+val DependencyHandler.`log4j-api-kotlin`: String
+    get() = "${Log4j.GROUP}:log4j-api-kotlin:1.0.0"
 val DependencyHandler.`log4j-bom`: String
     get() = "${Log4j.GROUP}:log4j-bom:${Log4j.VERSION}"
 val DependencyHandler.`log4j-core`: String
