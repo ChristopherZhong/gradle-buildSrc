a16670cf280e3662f45446a2464b41d95377f662
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-08-21 11:29:29 +0800

    convert javax.inject to new format

---
+++ src/main/kotlin/javax.inject.kt
@@ -0,0 +1,6 @@
+@file:Suppress("unused", "ObjectPropertyName")
+
+import org.gradle.api.artifacts.dsl.DependencyHandler
+
+val DependencyHandler.javax_inject: String
+    get() = "javax.inject:javax.inject:1"
