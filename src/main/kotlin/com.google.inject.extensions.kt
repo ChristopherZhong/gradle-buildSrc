74bd70bac4d56c6615ff6a9317ccd277c42b8e94
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-08-21 11:20:22 +0800

    convert guice extensions to new format

--- src/main/kotlin/com.google.inject.extensions.kt
+++ src/main/kotlin/com.google.inject.extensions.kt
@@ -1,12 +1,10 @@
+@file:Suppress("unused", "ObjectPropertyName")
+
 import org.gradle.api.artifacts.dsl.DependencyHandler
 
 object GuiceExtensions {
     const val GROUP = "${Guice.GROUP}.extensions"
-
-    object Dependencies {
-        const val ASSISTEDINJECT = "guice-assistedinject"
-    }
 }
 
 val DependencyHandler.`guice-assistedinject`: String
-    get() = "${GuiceExtensions.GROUP}:${GuiceExtensions.Dependencies.ASSISTEDINJECT}"
+    get() = "${GuiceExtensions.GROUP}:guice-assistedinject:4.2.2"
