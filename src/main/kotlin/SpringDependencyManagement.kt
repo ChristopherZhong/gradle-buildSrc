511c976b9c7781291328846cda1aa6da0ac77736
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-08-27 16:03:59 +0800

    inline all variables

--- src/main/kotlin/SpringDependencyManagement.kt
+++ src/main/kotlin/SpringDependencyManagement.kt
@@ -1,18 +1,10 @@
-import org.gradle.kotlin.dsl.version
+@file:Suppress("ObjectPropertyName", "unused", "FunctionName")
+
 import org.gradle.plugin.use.PluginDependenciesSpec
 import org.gradle.plugin.use.PluginDependencySpec
 
-object SpringDependencyManagement {
-    const val ID = "io.spring.dependency-management"
-    const val VERSION = "1.0.6.RELEASE"
-}
-
 val PluginDependenciesSpec.`spring-dependency-management`: PluginDependencySpec
-    get() = `spring-dependency-management`(SpringDependencyManagement.VERSION)
+    get() = `spring-dependency-management`("1.0.6.RELEASE")
 
 fun PluginDependenciesSpec.`spring-dependency-management`(version: String? = null): PluginDependencySpec =
-        id(SpringDependencyManagement.ID).apply {
-            if (version != null) {
-                return this version version
-            }
-        }
+        id("io.spring.dependency-management").applyVersion(version)
