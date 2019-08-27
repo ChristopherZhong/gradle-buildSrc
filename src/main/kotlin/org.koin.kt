2d168be82cf600d3ed41ec23ad922ec06e4d555c
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-08-27 15:56:43 +0800

    inline module names

--- src/main/kotlin/org.koin.kt
+++ src/main/kotlin/org.koin.kt
@@ -1,22 +1,17 @@
+@file:Suppress("unused", "ObjectPropertyName")
+
 import org.gradle.api.artifacts.dsl.DependencyHandler
 
 object Koin {
     const val GROUP = "org.koin"
     const val VERSION = "1.0.2"
-
-    internal object Dependencies {
-        const val CORE = "koin-core"
-        const val CORE_EXT = "koin-core-ext"
-        const val JAVA = "koin-java"
-        const val TEST = "koin-test"
-    }
 }
 
 val DependencyHandler.`koin-core`: String
-    get() = "${Koin.GROUP}:${Koin.Dependencies.CORE}:${Koin.VERSION}"
+    get() = "${Koin.GROUP}:koin-core:${Koin.VERSION}"
 val DependencyHandler.`koin-core-ext`: String
-    get() = "${Koin.GROUP}:${Koin.Dependencies.CORE_EXT}:${Koin.VERSION}"
+    get() = "${Koin.GROUP}:koin-core-ext:${Koin.VERSION}"
 val DependencyHandler.`koin-java`: String
-    get() = "${Koin.GROUP}:${Koin.Dependencies.JAVA}:${Koin.VERSION}"
+    get() = "${Koin.GROUP}:koin-java:${Koin.VERSION}"
 val DependencyHandler.`koin-test`: String
-    get() = "${Koin.GROUP}:${Koin.Dependencies.TEST}:${Koin.VERSION}"
+    get() = "${Koin.GROUP}:koin-test:${Koin.VERSION}"
