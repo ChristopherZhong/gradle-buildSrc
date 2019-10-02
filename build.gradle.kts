46c013fd16792180ecc9adbc42482b7d3010d2a6
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-10-02 13:03:57 +0800

    bump Gradle to 5.6.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,20 +6,7 @@ repositories {
     jcenter()
 }
 
-gradlePlugin {
-    plugins {
-        register("project-info") {
-            id = "project-info"
-            implementationClass = "ProjectInfoPlugin"
-        }
-        register("gradle-typed-plugins-and-dependencies") {
-            id = "gradle-typed-plugins-and-dependencies"
-            implementationClass = "GradleTypedPluginsAndDependenciesPlugin"
-        }
-    }
-}
-
 tasks.wrapper {
     distributionType = Wrapper.DistributionType.ALL
-    gradleVersion = "5.6"
+    gradleVersion = "5.6.2"
 }
