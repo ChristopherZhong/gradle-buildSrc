25375070d6626aa854f64d3893e9a875cf55e833
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2020-06-13 10:48:09 +0800

    Bump httpcore to 4.4.13

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -23,7 +23,7 @@ internal object Versions {
     const val GRAPHQL_JAVA_TOOLS = "5.2.4"
     const val GRAPHQL_JAVA_SPRING_BOOT_STARTER_WEBFLUX = "1.0"
     const val GRAPHQL_SPRING_BOOT_STARTER = "5.0.2"
-    const val HTTPCORE = "4.4.10"
+    const val HTTPCORE = "4.4.13"
     const val JACKSON = "2.11.0"
     val JAVA = JavaVersion.VERSION_11
     const val JAVAFX = "11.0.1"
