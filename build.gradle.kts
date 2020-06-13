ca5a03917cebb137211d1b631835d5516ef9473e
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2020-06-13 09:58:00 +0800

    Use all plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,20 @@
 plugins {
+    bintray() apply false
+    detekt() apply false
+    dokka() apply false
     `gradle-versions-plugin`()
+    `kotlin-jvm`() apply false
+    `kotlin-kapt`() apply false
+    `nebula-kotlin`() apply false
+    `nebula-javadoc-jar`() apply false
+    `nebula-maven-publish`() apply false
+    `nebula-publish-verification`() apply false
+    `nebula-release`() apply false
+    `nebula-source-jar`() apply false
+    `shadow-jar`() apply false
+    `shipkit-java`() apply false
+    `spring-boot`() apply false
+    `spring-dependency-management`() apply false
 }
 
 repositories {
