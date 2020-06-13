78765ddcb2a7fd3a653898ca65715e598e4f2128
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2020-06-13 12:11:54 +0800

    Bump log4j to 2.13.3

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -35,7 +35,7 @@ internal object Versions {
     const val KOIN = "1.0.2"
     const val KOTLIN = "1.3.72"
     const val KOTLINX = "1.3.1"
-    const val LOG4J = "2.11.2"
+    const val LOG4J = "2.13.3"
     const val LOG4J_KOTLIN = "1.0.0"
     const val LOMBOK = "1.18.4"
     const val MOCKITO = "3.0.0"
