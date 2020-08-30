ba7e2f791c05ab405e18401f5feca0ca787781c6
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2020-08-30 17:56:44 +0800

    Bump nebula release to 15.1.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -42,7 +42,7 @@ internal object Versions {
     const val MOCKK = "1.10.0"
     const val MOCK_FTP_SERVER = "2.7.1"
     const val NEBULA_PUBLISH = "17.3.2"
-    const val NEBULA_RELEASE = "15.0.2"
+    const val NEBULA_RELEASE = "15.1.0"
     const val OK_HTTP = "4.8.1"
     const val ORG_ECLIPSE_JDT_ANNOTATION = "2.2.400"
     const val PICOCLI = "4.5.1"
