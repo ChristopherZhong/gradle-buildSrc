d93d6baaebbbcfe3c604310a7cbe69c24d5bfc02
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2020-08-30 17:29:33 +0800

    Bump Spring Boot to 2.3.3.RELEASE

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -51,7 +51,7 @@ internal object Versions {
     const val SHIPKIT = "2.3.1"
     const val SLF4J_API = "1.7.25"
     const val SPRING = "5.1.9.RELEASE"
-    const val SPRING_BOOT = "2.3.1.RELEASE"
+    const val SPRING_BOOT = "2.3.3.RELEASE"
     const val SPRING_DEPENDENCY_MANAGEMENT = "1.0.9.RELEASE"
     const val STRIPE_JAVA = "19.45.0"
     const val TRUTH = "1.0.1"
