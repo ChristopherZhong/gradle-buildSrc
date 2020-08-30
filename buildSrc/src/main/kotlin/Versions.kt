c9cd9a6877782d3d1de5adf4c9e1ad49d72429c8
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2020-08-30 17:28:44 +0800

    Bump Picocli to 4.5.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -45,7 +45,7 @@ internal object Versions {
     const val NEBULA_RELEASE = "15.0.2"
     const val OK_HTTP = "4.8.1"
     const val ORG_ECLIPSE_JDT_ANNOTATION = "2.2.400"
-    const val PICOCLI = "4.3.2"
+    const val PICOCLI = "4.5.1"
     const val RXJAVA = "2.2.4"
     const val SHADOW_JAR = "6.0.0"
     const val SHIPKIT = "2.3.1"
